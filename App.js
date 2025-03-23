7f852e9cc71c57dda72fb9a403dde9621df6d602
Alex Nascimento <alexjose68@hotmail.com>
2025-03-23 20:52:28 -0300

    feat: Criando e entendendo componentes React, e treinando mais o conceito de props.

--- App.js
+++ App.js
@@ -3,19 +3,26 @@ import { View, Text, Image } from 'react-native';
 
 export default function App() {
   let nome = "Alex Nascimento";
-  let img = "https://sujeitoprogramador.com/reactlogo.png";
 
   return (
     <View>
       <Text style={{ marginTop: 25 }}>Olá Mundo</Text>
       <Text style={{ color: "#FF0000", fontSize: 25, margin: 15 }} >Meu segundo App</Text>
       <Text style={{ fontSize: 18, color: "green", }} >Sujeito Programador</Text>
-      <Image
-        style={{ width: 300, height: 300 }}
-        source={{ uri: img }}
-      />
 
-      <Text style={{ fontSize: 19 }}>{nome}</Text>
+      <Logo largura={350} altura={350} fulano={nome} profissao="Programador" />
+
+    </View>
+  )
+}
+
+function Logo(props) {
+  let img = "https://sujeitoprogramador.com/reactlogo.png";
+
+  return (
+    <View>
+      <Image source={{ uri: img }} style={{ width: props.largura, height: props.altura }} />
+      <Text style={{ fontSize: 19, fontWeight: "bold" }}>{props.fulano} - {props.profissao}.</Text>
     </View>
   )
 }
