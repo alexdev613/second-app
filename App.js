a22d4cca42fc9456e77cdb8d320f90b929e77993
Alex Nascimento <alexjose68@hotmail.com>
2025-03-23 21:24:53 -0300

    feat: Criando a estrutura de estados com o useState do React.

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React, { Component } from "react";
-import { View, Text, Image } from 'react-native';
+import React, { useState, Component } from "react";
+import { View, Text, Image, Button } from 'react-native';
 
 export default function App() {
-  let nome = "Alex Nascimento";
+  let name = "Alex Nascimento";
+
+  const [nome, setNome] = useState("Alex");
+  const [idade, setIdade] = useState(20);
+
+  function entrar(nome, idade) {
+    setNome(nome);
+    setIdade(idade);
+  }
 
   return (
-    <View>
-      <Text style={{ marginTop: 25 }}>Olá Mundo</Text>
-      <Text style={{ color: "#FF0000", fontSize: 25, margin: 15 }} >Meu segundo App</Text>
-      <Text style={{ fontSize: 18, color: "green", }} >Sujeito Programador</Text>
+    <View style={{ marginTop: 25 }}>
+      <Button title="Mudar nome" onPress={() => entrar("Alex Nascimento!", 37)} />
 
-      <Logo largura={350} altura={350} fulano={nome} profissao="Programador" />
+      <Text style={{ color: "#FF0000", fontSize: 25, margin: 10 }} >Meu segundo App - Olá Mundo!</Text>
+      
+      <Text style={{ fontSize: 18, color: "green", }} >Nome alterável: {nome}</Text>
+      <Text style={{ fontSize: 19, color: "blue", }} >Idade alterável: {idade}</Text>
+
+      <Logo largura={350} altura={350} fulano={name} profissao="Programador" />
 
     </View>
   )
@@ -20,7 +31,9 @@ function Logo(props) {
   let img = "https://sujeitoprogramador.com/reactlogo.png";
 
   return (
-    <View>
+    <View style={{ marginTop: 30}}>
+      <Text>-----------------------------------------------------------------------------------</Text>
+      <Text style={{ fontSize: 25, fontWeight: "bold" }} >COMPONENTE LOGO:</Text>
       <Image source={{ uri: img }} style={{ width: props.largura, height: props.altura }} />
       <Text style={{ fontSize: 19, fontWeight: "bold" }}>{props.fulano} - {props.profissao}.</Text>
     </View>
